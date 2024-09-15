e1df10d18a8b66af990236e161e8abafbc3e8fd2
Wellington Ramos <wellingtonjsramos@hotmail.com>
2024-09-15 12:52:52 -0300

    feat(service-items): added services

--- src/components/ServiceItems.js
+++ src/components/ServiceItems.js
@@ -1,4 +1,3 @@
-// src/components/ServiceItems.js
 import React from 'react';
 
 const ServiceItems = () => {
@@ -7,39 +6,54 @@ const ServiceItems = () => {
 			<div className="col-md-6 col-lg-4 wow fadeInUp" data-wow-delay="0.3s">
 				<div className="service-item p-4">
 					<div className="overflow-hidden mb-4">
-						<img className="img-fluid" src="img/service-4.jpg" alt="Train Freight" />
+						<img className="img-fluid" src="assets/img/corporate-trip.jpg" alt="Viagens Corporativas" />
 					</div>
-					<h4 className="mb-3">Train Freight</h4>
-					<p>Stet stet justo dolor sed duo. Ut clita sea sit ipsum diam lorem diam.</p>
-					<a className="btn-slide mt-2" href="www.google.com">
-						<i className="fa fa-arrow-right"></i>
-						<span>Read More</span>
+					<h4 className="mb-3">Viagens Corporativas</h4>
+					<p>Oferecemos transporte executivo para reuniões, eventos e viagens de negócios com total conforto, segurança e pontualidade.</p>
+					<a 
+						className="btn-slide mt-2" 
+						href="https://wa.me/5541998765432?text=Olá, gostaria de agendar uma Viagem Corporativa." 
+						target="_blank" 
+						rel="noopener noreferrer"
+					>
+						<i className="fab fa-whatsapp"></i>
+						<span>Agende Agora</span>
 					</a>
 				</div>
 			</div>
 			<div className="col-md-6 col-lg-4 wow fadeInUp" data-wow-delay="0.5s">
 				<div className="service-item p-4">
 					<div className="overflow-hidden mb-4">
-						<img className="img-fluid" src="img/service-5.jpg" alt="Customs Clearance" />
+						<img className="img-fluid" src="assets/img/aeroport-trip.jpg" alt="Traslados Aeroportuários" />
 					</div>
-					<h4 className="mb-3">Customs Clearance</h4>
-					<p>Stet stet justo dolor sed duo. Ut clita sea sit ipsum diam lorem diam.</p>
-					<a className="btn-slide mt-2" href="www.google.com">
-						<i className="fa fa-arrow-right"></i>
-						<span>Read More</span>
+					<h4 className="mb-3">Traslados Aeroportuários</h4>
+					<p>Serviço de traslado com conforto e pontualidade, garantindo que você chegue ao aeroporto a tempo e com tranquilidade.</p>
+					<a 
+						className="btn-slide mt-2" 
+						href="https://wa.me/5541998765432?text=Olá, gostaria de agendar um Traslado Aeroportuário." 
+						target="_blank" 
+						rel="noopener noreferrer"
+					>
+						<i className="fab fa-whatsapp"></i>
+						<span>Agende Agora</span>
 					</a>
 				</div>
 			</div>
 			<div className="col-md-6 col-lg-4 wow fadeInUp" data-wow-delay="0.7s">
 				<div className="service-item p-4">
 					<div className="overflow-hidden mb-4">
-						<img className="img-fluid" src="img/service-6.jpg" alt="Warehouse Solutions" />
+						<img className="img-fluid" src="assets/img/leisure-trip.jpg" alt="Viagens de Lazer" />
 					</div>
-					<h4 className="mb-3">Warehouse Solutions</h4>
-					<p>Stet stet justo dolor sed duo. Ut clita sea sit ipsum diam lorem diam.</p>
-					<a className="btn-slide mt-2" href="www.google.com">
-						<i className="fa fa-arrow-right"></i>
-						<span>Read More</span>
+					<h4 className="mb-3">Viagens de Lazer</h4>
+					<p>Seja para passeios ou viagens de longa distância, oferecemos transporte particular para garantir uma experiência agradável.</p>
+					<a 
+						className="btn-slide mt-2" 
+						href="https://wa.me/5541998765432?text=Olá, gostaria de agendar uma Viagem de Lazer." 
+						target="_blank" 
+						rel="noopener noreferrer"
+					>
+						<i className="fab fa-whatsapp"></i>
+						<span>Agende Agora</span>
 					</a>
 				</div>
 			</div>
