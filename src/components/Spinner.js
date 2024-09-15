3411673cd3d85e83c44bfc9d79457dcb048a89b5
Wellington Ramos <wellingtonjsramos@hotmail.com>
2024-09-15 15:13:41 -0300

    fix(spinner): spinner no more break on load

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,12 +1,30 @@
 // src/components/Spinner.js
-import React from 'react';
+import React, { useEffect } from 'react';
+import $ from 'jquery';
 
-const Spinner = () => (
-  <div id="spinner" className="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
-    <div className="spinner-grow text-primary" style={{ width: '3rem', height: '3rem' }} role="status">
-      <span className="sr-only">Loading...</span>
+const Spinner = () => {
+  useEffect(() => {
+    const spinner = () => {
+      setTimeout(() => {
+        if ($('#spinner').length > 0) {
+          $('#spinner').removeClass('show');
+        }
+      }, 1);
+    };
+    
+    spinner();
+
+    return () => {
+    };
+  }, []);
+
+  return (
+    <div id="spinner" className="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
+      <div className="spinner-grow text-primary" style={{ width: '3rem', height: '3rem' }} role="status">
+        <span className="sr-only">Loading...</span>
+      </div>
     </div>
-  </div>
-);
+  );
+}
 
 export default Spinner;
