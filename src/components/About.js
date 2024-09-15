cf01cd80ddfd081128d9a4bce0bdebaacb6c8efa
Wellington Ramos <wellingtonjsramos@hotmail.com>
2024-09-15 15:18:26 -0300

    fix(about): adjust number of trips

--- src/components/About.js
+++ src/components/About.js
@@ -14,7 +14,7 @@ const About = () => (
           <h6 className="text-secondary text-uppercase mb-3">Sobre Nós</h6>
           <h1 className="mb-5">Serviço de Transporte Particular com Excelência</h1>
           <p className="mb-5">
-            Com mais de 8 anos de experiência e mais de 30 mil viagens realizadas, a <strong>RLCO Transporte Particular</strong>, fundada por Ricardo Leandro da Costa Oliveira, é sinônimo de confiança, pontualidade e profissionalismo. Atuando em Curitiba e região, oferecemos um serviço de transporte particular com a qualidade que você merece.
+            Com mais de 8 anos de experiência e mais de 5 mil viagens realizadas, a <strong>RLCO Transporte Particular</strong>, fundada por Ricardo Leandro da Costa Oliveira, é sinônimo de confiança, pontualidade e profissionalismo. Atuando em Curitiba e região, oferecemos um serviço de transporte particular com a qualidade que você merece.
             <br /><br />
             Ricardo, um motorista altamente capacitado e experiente, já fez parceria com as principais plataformas de transporte como Uber e 99, adquirindo vasta experiência em oferecer um serviço de excelência, sempre com foco na satisfação do cliente. Seja para compromissos diários ou viagens especiais, garantimos um transporte rápido, seguro e confortável.
             <br /><br />
